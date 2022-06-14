bd95532ef8a60170b71d3cb47c43e21f6b7f5084
QC2168 <huangxinhong2125@gmail.com>
2022-06-14 18:16:59 +0800

    feat: javaScript列表显示

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,6 +13,7 @@ export default defineConfig4CustomTheme({
 
     themeConfig: {
         navbar: true,
+        logo: '/assets/avatar.jpg',
         authorAvatar: '/assets/avatar.jpg',
         author: '_island',
         sidebar: {
@@ -22,7 +23,14 @@ export default defineConfig4CustomTheme({
                 collapsable: true, // 可选的, 默认值是 true,
                 sidebarDepth: 1,    // 可选的, 默认值是 1
                 children: getChildren(path.join(__dirname, '../article'), 'article')
-            },]
+            },],
+            "/note/JavaScript/": [{
+                title: '文章列表',   // 必要的
+                path: '/JavaScript/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                collapsable: true, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: getChildren(path.join(__dirname, '../note/JavaScript'), 'note/JavaScript')
+            }],
         },
         // 404 腾讯公益
         noFoundPageByTencent: false,
@@ -33,7 +41,7 @@ export default defineConfig4CustomTheme({
             {
                 text: '笔记',
                 items: [
-                    { text: 'javascript', link: 'note/JavaScript/index' }
+                    { text: 'javascript', link: '/note/JavaScript/' }
                 ]
             }
         ]
