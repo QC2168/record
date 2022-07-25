a6220eb1de60c963691c468893a5158a6e911bc5
山田 <huangxinhong2125@gmail.com>
2022-07-25 22:54:54 +0800

    fix(build): 匹配不到文件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -24,13 +24,13 @@ export default defineConfig4CustomTheme({
                 sidebarDepth: 1,    // 可选的, 默认值是 1
                 children: getChildren(path.join(__dirname, '../article'), 'article')
             },],
-            "/note/JavaScript/": [{
-                title: '文章列表',   // 必要的
-                path: '/JavaScript/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-                collapsable: true, // 可选的, 默认值是 true,
-                sidebarDepth: 1,    // 可选的, 默认值是 1
-                children: getChildren(path.join(__dirname, '../note/JavaScript'), 'note/JavaScript')
-            }],
+            // "/note/JavaScript/": [{
+            //     title: '文章列表',   // 必要的
+            //     path: '/JavaScript/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+            //     collapsable: true, // 可选的, 默认值是 true,
+            //     sidebarDepth: 1,    // 可选的, 默认值是 1
+            //     children: getChildren(path.join(__dirname, '../note/JavaScript'), 'note/JavaScript')
+            // }],
         },
         // 404 腾讯公益
         noFoundPageByTencent: false,
@@ -38,12 +38,12 @@ export default defineConfig4CustomTheme({
             { text: '首页', link: '/' },
             { text: '文章', link: '/article/' },
             { text: '面试', link: '/interview/' },
-            {
-                text: '笔记',
-                items: [
-                    { text: 'javascript', link: '/note/JavaScript/' }
-                ]
-            }
+            // {
+            //     text: '笔记',
+            //     items: [
+            //         { text: 'javascript', link: '/note/JavaScript/' }
+            //     ]
+            // }
         ]
     }
 });
