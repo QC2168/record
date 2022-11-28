4972401f87324bbbcc5f327c7d8317ee3a8fdb4e
山田 <huangxinhong2125@gmail.com>
2022-11-28 23:23:07 +0800

    滚动条样式

--- docs/.vitepress/theme/MyLayout.vue
+++ docs/.vitepress/theme/MyLayout.vue
@@ -23,6 +23,24 @@ const { frontmatter } = data;
     <template #aside-outline-before>目录</template>
   </Layout>
 </template>
-<style scoped>
+<style>
 @import "./index.css";
+
+/* 滚动槽 */
+::-webkit-scrollbar {
+    width: 6px;
+    height: 6px;
+}
+::-webkit-scrollbar-track {
+    border-radius: 3px;
+    background: rgba(0,0,0,0.06);
+    -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.08);
+}
+/* 滚动条滑块 */
+::-webkit-scrollbar-thumb {
+    border-radius: 3px;
+    background: rgba(0,0,0,0.12);
+    -webkit-box-shadow: inset 0 0 10px rgba(0,0,0,0.2);
+}
+
 </style>
